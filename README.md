7871dc18773f101391ccbfb7789d38a5ea7c4a15
xf <1294699027@qq.com>
2021-01-23 14:59:44 +0800

    edit admin

--- README.md
+++ README.md
@@ -1,24 +1,26 @@
-# mall-app
+# 每日优鲜B端项目
 
-## Project setup
-```
+技术栈：vue全家桶 + axios + antd for vue
+
+实现了登录、注册、忘记密码，产品分页获取，编辑、删除、添加，产品类目的编辑、添加、删除。
+
+不同身份（customer、admin）的用户权限（路由）不同。
+
+## 启动方式
+```shell
+
 npm install
-```
 
-### Compiles and hot-reloads for development
-```
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## 项目预览
+![](./assets/login.png)
 
-### Lints and fixes files
-```
-npm run lint
-```
+![](./assets/list.png)
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+![](./assets/edit.png)
+
+![](./assets/add.png)
+
+![](./assets/category.png)
--- src/validateRules/login.js
+++ src/validateRules/login.js
@@ -1,26 +1,17 @@
 const validateEmail = (rule, value, callback) => {
+  if (!value) {
+    callback(new Error("请输入邮箱"));
+  }
   const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (!reg.test(value)) {
     callback(new Error("邮箱格式不正确"));
   }
   callback();
 };
-const validatePwd = (rule, value, callback) => {
-  if (value === "") {
-    callback(new Error("请输入密码"));
-  }
-  callback();
-};
 
 export const loginRules = {
-  email: [
-    { validator: validateEmail, trigger: "change" },
-    { required: true, trigger: "blur", message: "请输入邮箱" }
-  ],
-  password: [
-    { validator: validatePwd, trigger: "change" },
-    { required: true, trigger: "blur", message: "请输入密码" }
-  ]
+  email: [{ validator: validateEmail }],
+  password: [{ required: true, message: "请输入密码" }]
 };
 
 export const registerRules = {
